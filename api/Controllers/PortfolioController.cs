d059fa4a1bc8936e04abd442fc9751a8ea6aa840
heroband <dyalist2001@gmail.com>
2024-07-26 14:25:20 +0300

    created portfolio delete method

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -63,5 +63,24 @@ namespace api.Controllers
             if(portfolioModel == null) return StatusCode(500, "Can't create.");
             else return Ok(portfolioModel);
         }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+
+            var filteredPortfolio = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower()).ToList();
+
+            if(filteredPortfolio.Count == 1) 
+            {
+                await _portfolioRepo.DeletePortfolio(appUser, symbol);
+            }
+            else return BadRequest("Stock not found in portfolio.");
+
+            return Ok();
+        }
     }
 }
